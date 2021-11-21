91c252c3ebc77aded7156b9f7d5209d0960eb5bc
Giovanni Pollo <giovannipollo98@gmail.com>
2021-11-21 18:35:00 +0100

    Added automator worflow

--- mission_control.sh
+++ mission_control.sh
@@ -6,3 +6,9 @@ defaults write com.apple.dock dashboard-in-overlay -bool true
 
 # Don’t automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
+
+# Don’t group windows by application in Mission Control
+defaults write com.apple.dock expose-group-apps -bool false
+
+# Speed up Mission Control animations
+defaults write com.apple.dock expose-animation-duration -float 0
